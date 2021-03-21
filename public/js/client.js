05ff7e0bce0d145c6a24eecc65ccf7f90fe1bc48
B-290 <bruk.gebremariam@edu.sait.ca>
2021-03-21 15:57:34 -0600

    v.f.c6

--- public/js/client.js
+++ public/js/client.js
@@ -9,16 +9,18 @@
       .then(function(response) {
          console.log(response);
         return response.json();
-})
+   })
 
-      .then(function(data) {
+      .then(function(data){
   // Any code that needs `data` should be in this code block.
          console.log(data);
+        
+   })
   
-  
-        data.forEach(function(image){
+  data.forEach(function(image){
       
          console.log(image);
+  })      
        
         data.forEach(function(image){
         
@@ -27,11 +29,10 @@
         <img src='${image.fileName}' alt='my image'>
         <a href="${image.attribution.url}">Some Text</a>`;
 
-  
-      });
+   });
         document.querySelector('#table').innerHTML = someHTML;
 
-    });
+
        
     
 
